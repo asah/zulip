b70a822b3304382c8f73d02841e32ec5fd2574cc
Adam Sah <140002+asah@users.noreply.github.com>
2022-01-09 14:56:04 -0500

    remove hardcoded unread_count

--- static/js/notifications.js
+++ static/js/notifications.js
@@ -148,8 +148,7 @@ export function update_unread_counts(new_unread_count, new_pm_count) {
     pm_count = new_pm_count;
 
     // Indicate the message count in the favicon
-//    favicon.update_favicon(unread_count, pm_count);
-    favicon.update_favicon(0,0);
+    favicon.update_favicon(unread_count, pm_count);
 
     // Notify the current desktop app's UI about the new unread count.
     if (window.electron_bridge !== undefined) {
