3d1bf8b445106ff2dfc18fd153f268f7107d1f44
Adam Sah <140002+asah@users.noreply.github.com>
2022-03-04 15:38:48 -0400

    bugfix for cutoff

--- zerver/lib/digest.py
+++ zerver/lib/digest.py
@@ -360,7 +360,7 @@ def bulk_get_digest_context(users: List[UserProfile], cutoff: float) -> Dict[int
         cutoff_date = timezone_now() - datetime.timedelta(days = 365)
         hotwords_date = timezone_now() - datetime.timedelta(days = 365)
     else:
-        cutoff_date = timezone_now() - datetime.timedelta(days = int(cutoff))
+        cutoff_date = int(cutoff)
         hotwords_date = timezone_now() - datetime.timedelta(days = int(HOTWORDS_CUTOFF))
     yesterday = timezone_now() - datetime.timedelta(hours = 24)
 
